e8ab20284dd4bf46a73cf4f273a6d1d123f9b7d9
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-18 18:21:17 +0500

    v4

--- models/region.js
+++ models/region.js
@@ -2,20 +2,19 @@ const { DataTypes } = require("sequelize");
 const { db } = require("../config/db");
 
 const Region = db.define(
-    "Region",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  "Region",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    { timestamps: false }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
 );
 
-
-module.exports = Region
+module.exports = Region;
