25b982e46c8bfb2c03ccf9b188bdb42a15b48579
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-18 16:58:07 +0500

    v1

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const logger = require("./logger");
-
-const app = express();
-app.use(express.json());
-
-app.listen(3001, () => console.log("server is running on 3001"));
