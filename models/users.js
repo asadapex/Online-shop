69c2584aab7b8fecfb0764e753707dfe1d8d3465
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-18 18:00:45 +0500

    auth

--- models/users.js
+++ models/users.js
@@ -11,4 +11,38 @@ const User = db.define("Users", {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  region_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
+
+module.exports = User;
